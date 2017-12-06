949ab216425ac5d9d43f1ce1e70d6d0769be05a7
Simon Cozens <simon@simon-cozens.org>
2017-12-06 14:20:02 +1100

    Make this simpler and, simultaneously, work.

--- src/app/pages/editprofile/editprofile.ts
+++ src/app/pages/editprofile/editprofile.ts
@@ -1,5 +1,4 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
-import { FormsModule, FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { PersonService } from '../../services/person.service';
 import { Person, Affiliation, FieldPermissions } from '../../classes/person';
@@ -17,48 +16,23 @@ import { picklists } from "../../picklists";
   templateUrl: './editprofile.component.html'
 })
 export class EditProfileComponent implements OnInit {
-    public profileForm: FormGroup;
-
   person: Person;  public alerts: any = [];
   dirty: boolean;
   picklists = picklists;
 
   constructor(public personService: PersonService,
-    private _fb: FormBuilder, private auth: AuthService,
+    private auth: AuthService,
     public alertCtrl: AlertController,
     public toastCtrl: ToastController) {
     this.person = this.auth.loggedInUser();
     this.dirty = false;
   }
   ngOnInit() {
-    const controls = {
-          intro_bio: this.person.intro_bio,
-          preferred_contact: this.person.preferred_contact,
-          affiliations: this._fb.array(
-            this.person.affiliations.map(a => this.initAffiliation(a))
-          )
-    };
-    this.profileForm = this._fb.group(controls);
-  }
-
-  initAffiliation(a: Affiliation = <Affiliation>{}) {
-    return this._fb.group({
-        organisation: [a.organisation],
-        position: [a.position],
-        website: [a.website]
-    });
-  }
-
-  getAffiliations(profileForm): FormArray {
-    return profileForm.get('affiliations').controls;
   }
 
   save() {
-    let value = this.profileForm.getRawValue()
-    value.field_permissions = this.person.field_permissions
-    this.personService.saveProfile(value).then(
+    this.personService.saveProfile(this.person).then(
       response => {
-        this.person = Object.assign(this.person, value);
         this.auth.setLoggedInUser(this.person);
         const toast = this.toastCtrl.create({
             message: 'Saved successfully',
@@ -78,27 +52,6 @@ export class EditProfileComponent implements OnInit {
     }
 
     )
-  }
-
-  showPrefContactRadio() {
-    let alert = this.alertCtrl.create();
-    alert.setTitle('Preferred method of contact');
-
-    for (var n of ["email", "skype", "whatsapp"]) {
-      alert.addInput({
-        type: 'radio', label: n, value: n, checked: this.person.preferred_contact == n
-      });
-    }
-
-    alert.addButton('Cancel');
-    alert.addButton({
-      text: 'OK',
-      handler: data => {
-        this.person.preferred_contact = data
-        if (data != this.person.preferred_contact) { this.dirty = true }
-      }
-    });
-    alert.present();
   }
 
   showPermissions(field) {
